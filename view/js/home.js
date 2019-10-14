8a3af45cb5a90c39e02fd31d37e53a0a94539571
ulisesten <ulises.mtz.el@gmail.com>
2019-10-14 11:21:53 -0500

    responsive designe

--- view/js/home.js
+++ view/js/home.js
@@ -13,9 +13,11 @@ socket.on('message', (data) => {
     console.log( mUsername, mId );
 
     if( data.sender === mId)
-        messages.append(newSenderMessage(data.message));
+        messages.append(newSenderMessage(data));
     else
-        messages.append(newIncomingMessage(data.message));
+        messages.append(newIncomingMessage(data));
+
+    messages.scrollTo(0,messages.scrollHeight);
 })
 
 
@@ -36,23 +38,48 @@ sendMessage.addEventListener('click', () => {
     socket.emit('message', messageObject );
 });
 
-function newSenderMessage(message){
+function newSenderMessage(data){
+    var div = newEl('div');
+    div.setAttribute('class','bubble-container');
 
     var row = newEl('li');
     row.setAttribute('class','senderMessage');
-    row.innerHTML = message;
-    console.log(message);
+    row.innerHTML = data.message;
 
-    return row;
+    var dateSpan = newEl('div');
+    dateSpan.setAttribute('class', 'timestamp-right')
+    dateSpan.innerHTML = data.timestamp;
+
+    div.append(row);
+    div.append(dateSpan);
+
+    return div;
 }
 
-function newIncomingMessage(message){
+function newIncomingMessage(data){
+    var div = newEl('div');
+    var image = newEl('img');
+    image.setAttribute('src', 'https://res.cloudinary.com/djlzeapiz/image/upload/c_scale,w_30/v1571063915/profile/userprofile2.jpg')
+    image.setAttribute('class','profileImage');
+
+    var nameSpan = newEl('span')
+    nameSpan.setAttribute('class', 'username')
+    nameSpan.innerHTML = data.username;
+
     var row = newEl('li');
     row.setAttribute('class','incomingMessage');
-    row.innerHTML = message;
-    console.log(message);
+    row.innerHTML = data.message;
+    //console.log(message);
+    var dateSpan = newEl('span');
+    dateSpan.setAttribute('class', 'timestamp')
+    dateSpan.innerHTML = data.timestamp;
 
-    return row;
+    div.append(image);
+    div.append(nameSpan);
+    div.append(row)
+    div.append(dateSpan)
+
+    return div;
 }
 
 
