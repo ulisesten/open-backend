686fa334df3aa246f2e3a382f77a596ff7c163f7
Ulises <ulises.mtz.el@gmail.com>
2020-03-15 13:46:34 -0600

    Adding manage to url spaces

--- build/urlparser.target.mk
+++ build/urlparser.target.mk
@@ -7,8 +7,13 @@ DEFS_Debug := \
 	'-DUSING_UV_SHARED=1' \
 	'-DUSING_V8_SHARED=1' \
 	'-DV8_DEPRECATION_WARNINGS=1' \
+	'-DV8_DEPRECATION_WARNINGS' \
+	'-DV8_IMMINENT_DEPRECATION_WARNINGS' \
 	'-D_LARGEFILE_SOURCE' \
 	'-D_FILE_OFFSET_BITS=64' \
+	'-D__STDC_FORMAT_MACROS' \
+	'-DOPENSSL_NO_PINSHARED' \
+	'-DOPENSSL_THREADS' \
 	'-DBUILDING_NODE_EXTENSION' \
 	'-DDEBUG' \
 	'-D_DEBUG' \
@@ -35,21 +40,26 @@ CFLAGS_CC_Debug := \
 	-std=gnu++1y
 
 INCS_Debug := \
-	-I/home/ulises/.cache/node-gyp/10.16.0/include/node \
-	-I/home/ulises/.cache/node-gyp/10.16.0/src \
-	-I/home/ulises/.cache/node-gyp/10.16.0/deps/openssl/config \
-	-I/home/ulises/.cache/node-gyp/10.16.0/deps/openssl/openssl/include \
-	-I/home/ulises/.cache/node-gyp/10.16.0/deps/uv/include \
-	-I/home/ulises/.cache/node-gyp/10.16.0/deps/zlib \
-	-I/home/ulises/.cache/node-gyp/10.16.0/deps/v8/include
+	-I/home/ulises/.cache/node-gyp/13.8.0/include/node \
+	-I/home/ulises/.cache/node-gyp/13.8.0/src \
+	-I/home/ulises/.cache/node-gyp/13.8.0/deps/openssl/config \
+	-I/home/ulises/.cache/node-gyp/13.8.0/deps/openssl/openssl/include \
+	-I/home/ulises/.cache/node-gyp/13.8.0/deps/uv/include \
+	-I/home/ulises/.cache/node-gyp/13.8.0/deps/zlib \
+	-I/home/ulises/.cache/node-gyp/13.8.0/deps/v8/include
 
 DEFS_Release := \
 	'-DNODE_GYP_MODULE_NAME=urlparser' \
 	'-DUSING_UV_SHARED=1' \
 	'-DUSING_V8_SHARED=1' \
 	'-DV8_DEPRECATION_WARNINGS=1' \
+	'-DV8_DEPRECATION_WARNINGS' \
+	'-DV8_IMMINENT_DEPRECATION_WARNINGS' \
 	'-D_LARGEFILE_SOURCE' \
 	'-D_FILE_OFFSET_BITS=64' \
+	'-D__STDC_FORMAT_MACROS' \
+	'-DOPENSSL_NO_PINSHARED' \
+	'-DOPENSSL_THREADS' \
 	'-DBUILDING_NODE_EXTENSION'
 
 # Flags passed to all source files.
@@ -73,13 +83,13 @@ CFLAGS_CC_Release := \
 	-std=gnu++1y
 
 INCS_Release := \
-	-I/home/ulises/.cache/node-gyp/10.16.0/include/node \
-	-I/home/ulises/.cache/node-gyp/10.16.0/src \
-	-I/home/ulises/.cache/node-gyp/10.16.0/deps/openssl/config \
-	-I/home/ulises/.cache/node-gyp/10.16.0/deps/openssl/openssl/include \
-	-I/home/ulises/.cache/node-gyp/10.16.0/deps/uv/include \
-	-I/home/ulises/.cache/node-gyp/10.16.0/deps/zlib \
-	-I/home/ulises/.cache/node-gyp/10.16.0/deps/v8/include
+	-I/home/ulises/.cache/node-gyp/13.8.0/include/node \
+	-I/home/ulises/.cache/node-gyp/13.8.0/src \
+	-I/home/ulises/.cache/node-gyp/13.8.0/deps/openssl/config \
+	-I/home/ulises/.cache/node-gyp/13.8.0/deps/openssl/openssl/include \
+	-I/home/ulises/.cache/node-gyp/13.8.0/deps/uv/include \
+	-I/home/ulises/.cache/node-gyp/13.8.0/deps/zlib \
+	-I/home/ulises/.cache/node-gyp/13.8.0/deps/v8/include
 
 OBJS := \
 	$(obj).target/$(TARGET)/server/utils/addons/urlparser.o
